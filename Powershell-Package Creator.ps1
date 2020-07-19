149f68d87b1875ba442658cfd18b3d56659d585f
Raghu <kingraghu6@gmail.com>
2020-07-19 16:44:11 +0530

    Automate Package creator code

---
+++ Powershell-Package Creator.ps1
@@ -0,0 +1,26 @@
+anusha
+anusha@123
+
+gokul
+gokul@123
+
+sameer
+Sameer@123
+
+sunil
+Sunil@123
+
+
+Hi Anusha,
+
+    I had created new Dev login credential, which includes same roles, please try to login from the below mentioned username and pass and get back to me if you face any issue while login.
+
+Username: anusha
+Password:   anusha@123
+
+Thanks and Regards,
+Raghuraj T
+
+
+
+Dev Environment Login Credentials
